e0dc8fbd793e69d2afba717ffc84f8cdf0d4989e
Lucas Sa <88542169+saLucasRd@users.noreply.github.com>
2026-02-27 14:55:09 -0300

    treesitter fixes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,29 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function() 
-      local configs = require("nvim-treesitter")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function() 
+			local configs = require("nvim-treesitter")
 
-      configs.setup({
-        ensure_installed = { 
-            "lua", "vim", "vimdoc", "query", "javascript", 
-            "html", "css", "python", "bash", "java" 
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<A-o>",
-          node_incremental = "<A-o>",
-          scope_incremental = "<A-O>",
-          node_decremental = "<A-i>",
+			configs.setup({
+				ensure_installed = { 
+					"lua", "vim", "vimdoc", "query", "javascript", 
+					"html", "css", "python", "bash", "java" 
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					--keymaps = {
+					--	init_selection = "gnn",
+					--	node_incremental = "grn",
+					--	scope_incremental = "grc",
+					--	node_decremental = "grm",
 
-        },
-      })
-    end
-  }
+					--},
+				},
+			})
+		end
+	}
 }
--- lua/remap.lua
+++ lua/remap.lua
@@ -1,5 +1,2 @@
 vim.g.mapleader = " " -- leader key
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "[F]ile [E]xplorer (Netrw)" })
-vim.g.netrw_banner = 0         -- Remove o cabeçalho (instruções)
-vim.g.netrw_liststyle = 3      -- Modo árvore (tree view)
-vim.g.netrw_winsize = 25       -- Largura da lateral
