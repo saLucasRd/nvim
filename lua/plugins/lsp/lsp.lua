40932dd9c1f806a409be344be57b7373ea1bed5b
Lucas Sa <88542169+saLucasRd@users.noreply.github.com>
2026-03-04 14:54:27 -0300

    lsp

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -26,6 +26,40 @@ return {
 
 			vim.lsp.enable("lua_ls")
 
+			vim.lsp.config("jdtls", {
+				install = true,
+				cmd = { "jdtls" },
+				capabilities = capabilities,
+				root_dir = require("lspconfig.util").root_pattern("pom.xml", "gradle.build", ".git", "settings.gradle"),
+				settings = {
+					java = {
+						home = os.getenv("JAVA_HOME"),
+						eclipse = {
+							downloadSources = true,
+						},
+						configuration = {
+							updateBuildConfiguration = "interactive",
+						},
+						maven = {
+							downloadSources = true,
+						},
+						implementationCodeLens = {
+							enabled = true,
+						},
+						referencesCodeLens = {
+							enabled = true,
+						},
+						format = {
+							enabled = true,
+						},
+					},
+				},
+				init_option = {
+					bundles = {},
+				},
+			})
+			vim.lsp.enable("jdtls")
+
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
 				callback = function(ev)
@@ -37,11 +71,17 @@ return {
 					nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 					nmap("<leader>cl", vim.lsp.codelens.run, "[C]ode [L]ens")
 
-					nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-					nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-					nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+					nmap("<leader>gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+					nmap("<leader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					nmap("<leader>gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
 				end,
+			})
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = false,
 			})
 		end,
 	},
