dcdf189fdaf3a41ee977f46ad5536fe3cd8afbe4
fatkiwi <salucasrd@noreply.codeberg.org>
2026-06-25 15:24:32 -0300

    kanagawa-wave

--- lua/plugins/editor_ui.lua
+++ lua/plugins/editor_ui.lua
@@ -4,12 +4,11 @@ return {
 		opts = { default = true },
 	},
 	{
-		"ellisonleao/gruvbox.nvim",
+		"rebelot/kanagawa.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.o.background = "dark"
-			vim.cmd.colorscheme("gruvbox")
+			vim.cmd.colorscheme("kanagawa-wave")
 		end,
 	},
 	{
