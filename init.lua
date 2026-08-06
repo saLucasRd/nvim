d6061ffdb7bf1bbe82ffcd8e7206bc47919c9bb4
fatkiwi <salucasrd@noreply.codeberg.org>
2026-08-06 14:48:09 -0300

    mappings

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 require("config.mappings")
 require("config.options")
---
--- Bootstrap lazy.nvim
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
