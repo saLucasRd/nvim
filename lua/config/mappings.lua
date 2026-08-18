bb6e3d3acd64e9ce3d2ae325a1260a4c7c2a4ff2
fatkiwi <salucasrd@noreply.codeberg.org>
2026-08-18 15:31:17 -0300

    no idea of what im doing

--- lua/config/mappings.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " " -- leader key
-vim.g.maplocalleader = " "
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = 'Clear search highlight', silent = true }) -- limpar highlight de busca
-
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>" , { desc = 'Quick Save'})
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = 'Exec'}, { silent = true }) -- exec
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", { desc = 'Replace'}) -- replace
-
-
